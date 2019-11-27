ef4db689693cd6667691d8036bc66195f2bd8d77
Daniel Lutz <daniel.lutz@outlook.com>
2019-11-27 19:37:29 +0100

    code init

---
+++ src/HideKnowledgeItemFileFormHelpBox/HideKnowledgeItemFileFormHelpBoxAddon.cs
@@ -0,0 +1,23 @@
+﻿using SwissAcademic.Citavi.Shell;
+using System;
+
+namespace HideKnowledgeItemFileFormHelpBox
+{
+    public class HideKnowledgeItemFileFormHelpBoxAddon : CitaviAddOn<KnowledgeItemFileForm>
+    {
+        #region Methods
+
+        public override void OnHostingFormLoaded(KnowledgeItemFileForm knowledgeItemFileForm)
+        {
+            try
+            {
+                knowledgeItemFileForm.HelpBox.Hide();
+            }
+            catch (Exception){}
+
+            base.OnHostingFormLoaded(knowledgeItemFileForm);
+        }
+
+        #endregion
+    }
+}
